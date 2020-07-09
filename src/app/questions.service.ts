9cc854dd7b7d3e6772957b2447221617be68bcb6
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-07-09 19:09:51 +0200

    Updated with new quiz

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Quiz, Question } from './quiz.model';
 
@@ -10,9 +9,7 @@ import { Quiz, Question } from './quiz.model';
 export class QuestionsService {
   constructor(private http: HttpClient) { }
 
-  public getJSON(fileId: string) {
-    return this.http.get(`./assets/${fileId}.json`) as Observable<any>;
-    public getQuizzes() {
+  public getQuizzes() {
     return this.http.get(`./assets/quiz-list.json`).pipe(
       map((result: any[]) => {
         return result.map(
